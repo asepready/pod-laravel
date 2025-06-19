4ed355903846bcc857a17a58fbd73511d71da756
Asep Ready <savalunova@gmail.com>
2025-06-19 09:38:10 -0400

    laravel done

--- container/nginx/ssl/cert.sh
+++
@@ -1,1 +0,0 @@
-openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout self-signed.key -out self-signed.crt
--- scripts/lara.sh
+++
@@ -1,8 +0,0 @@
-# Remove old images
-podman rmi -f nginx 2>/dev/null || true
-
-# build images
-podman build -t nginx ./../containers/nginx
-
-podman run --name nginx -t \
-  -d nginx:stable-alpine-perl
